36e813637fddfb1af1b5bfcd379b2148a5b7d5b5
Kjetil Flovild-Midtlie <kfm@docstream.no>
2016-02-28 15:00:49 +0100

    mainHandler

--- start.scm
+++ start.scm
@@ -1,14 +1,22 @@
 #! /usr/bin/csi -script
 (require 'argsparse )
 (use spiffy)
+(use intarweb)
 
 
 (server-port (cdr (assoc 'port config)))
-(root-path "publ_test")
 
 ;; When dropping privileges, switch to this user
 ;(spiffy-user (cdr (assoc 'user config)))
 ;(spiffy-group (cdr (assoc 'user config)))
 
 (print "starting on port: " (server-port))
+
+(define (main/hnd config)
+  (lambda (continue)
+     (print (request-uri (current-request)))
+     (send-status 'ok config)))
+
+(vhost-map `( 
+              (".*" . ,(main/hnd "accepted"))))
 (start-server)
